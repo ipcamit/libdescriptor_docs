33e6c0d662857b7fda7e443f3a82e188108bd002
Amit Gupta <amit_@outlook.in>
2024-01-22 11:44:40 -0600

    dummy project 1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.sysconfig import get_config_vars
+from setuptools import find_packages, setup
+
+setup(
+    name="libdescriptor",
+    version="0.0.7",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="Amit Gupta",
+)
