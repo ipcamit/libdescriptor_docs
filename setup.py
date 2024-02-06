0e8108515dfeb2412fac4a8c661fac5ae84d1897
Amit Gupta <amit_@outlook.in>
2024-02-06 15:51:00 -0600

    Fixed missing images

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="libdescriptor",
-    version="0.0.7",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
     ],
